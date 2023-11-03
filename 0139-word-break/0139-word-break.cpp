fe0a28fbd1a6b8ca732eb8eabd57bbc2d827b22b
Viral Doshi <viraldoshi321@gmail.com>
2023-11-03 18:10:13 -0400

    Time: 0 ms (100%), Space: 7.9 MB (92.22%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    bool dp(string s, int i, vector<string>& words, vector<int>& memo){
-        if (i<0)
-            return true;
-        if (memo[i] != -1)
-            return memo[i] == 1;
-        for (string word: words){
-            int k = word.size();
-            if (i-k+1 < 0)
-                continue;
-            if (s.substr(i-k+1, k) == word && dp(s, i-k, words, memo)){
-                memo[i] = 1;
-                return true;
-            }
-        
-        }
-        memo[i] = 0;
-        return false;
-    }
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.size();
-        vector<int> memo(n, -1);
-        return dp(s, n-1, wordDict, memo);
+        vector<bool> dp(n, false);
+        for(int i=0; i<n; i++){
+            for(string word: wordDict){
+                int k = word.size();
+                if (i< k - 1)
+                    continue;
+                if (i == k-1 || dp[i-k])
+                    if (s.substr(i-k+1,k) == word){
+                        dp[i] = true;
+                        break;
+                    }
+            }
+        }
+        return dp[n-1];
     }
 };
